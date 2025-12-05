811692aea3ed4136cc13e220ed550f9d59a8b4ce
Clement Tsang <34804052+ClementTsang@users.noreply.github.com>
2025-12-05 17:26:16 -0500

    day 6 prep

---
+++ day_06/solution.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import sys
+from typing import List
+
+sys.setrecursionlimit(100000)
+FILE = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
+
+
+def read_lines_to_list() -> List[str]:
+    lines: List[str] = []
+    with open(FILE, "r", encoding="utf-8") as f:
+        for line in f:
+            line = line.strip()
+            # lines.append(list(line)) # Change the return to Line[Line[str]]
+            # lines.append([int(v) for v in list(line)])) # Change the signature to List[int]
+            lines.append(line)
+
+    return lines
+
+
+def part_one():
+    lines = read_lines_to_list()
+    answer = 0
+
+    print(f"Part 1: {answer}")
+
+
+def part_two():
+    lines = read_lines_to_list()
+    answer = 0
+
+    print(f"Part 2: {answer}")
+
+
+part_one()
+part_two()
